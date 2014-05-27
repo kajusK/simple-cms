150ea7262a88d315363bac8df475a491f4aabc2e
Jakub Kaderka <jakub.kaderka@gmail.com>
2014-05-27 16:51:14 +0200

    Paging moved from category to own model and view

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -13,9 +13,6 @@ class CategoryController extends Controller
 {
 	protected $view = "category";
 
-	private $cat_id = false;
-	private $cat_name = false;
-
 	/**
 	 * Generate page of articles
 	 *
@@ -51,8 +48,6 @@ class CategoryController extends Controller
 			$page = $param[2];
 		}
 
-		$this->cat_id = $param[0];
-		$this->cat_name = $name;
 		$this->_genPaging($param[0], $page, PER_PAGE);
 	}
 
@@ -93,8 +88,7 @@ class CategoryController extends Controller
 			return;
 		}
 
-		$from = ($page - 1)*$per_page;
-		if ($from < 0 || $from > $count - 1) {
+		if (($from = Paging::getFrom($page, $per_page, $count)) === false) {
 			$this->_notFound();
 			return;
 		}
@@ -108,49 +102,10 @@ class CategoryController extends Controller
 		foreach ($this->data['articles'] as &$a)
 			$a['link'] = Url::getFrom($temp, $a['id'], $a['url']);
 
-		$this->_genNav($count, $page, $per_page);
-	}
-
-	/**
-	 * Generate page navigation
-	 *
-	 * @param int $count number of articles in category
-	 * @param int $page
-	 * @param int $per_page articles per one page
-	 */
-	private function _genNav($count, $page, $per_page) {
-		$pages = ceil($count / $per_page);
-		if ($pages <= 1) {
-			$this->data['nav'] = false;
-			return;
-		}
-
-		//prepare link
-		$url_temp = Url::getTemp("category", $this->cat_id, "page", "%d");
-
-		if ($page != 1) {
-			$this->data['first'] = Url::get("category", $this->cat_id);
-			$this->data['prev'] = Url::getFrom($url_temp, $page-1);
-		}
-
-		//calculate minimum page number to show
-		$first = $page - (int) NAV_MAX_COUNT/2;
-		if ($first < 1)
-			$first = 1;
-
-		//if close to the end, show NAV_MAX_COUNT pages
-		$diff = $first + NAV_MAX_COUNT - $pages; //pages over the last page
-		if ($diff > 0)
-			$first = ($first - $diff > 0) ? $first - $diff + 1 : 1;
-
-		$this->data['nav'] = array();
-		for ($i = $first; $i < $first + NAV_MAX_COUNT && $i <= $pages; $i++)
-			$this->data['nav'][$i] = $i == $page ? false : Url::getFrom($url_temp, $i);
-
-		if ($page < $pages) {
-			$this->data['next'] = Url::getFrom($url_temp, $page+1);
-			$this->data['last'] = Url::getFrom($url_temp, $pages);
-		}
+		/* gen page navigation */
+		$this->data['nav'] = Paging::genNav($count, $page, $per_page,
+				Url::get("category", $cat_id),
+				Url::getTemp("category", $cat_id, "page", "%d"));
 	}
 
 	private function _empty() {
---
+++ models/Paging.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Yet another simple CMS
+ *
+ * @copyright 2014 Jakub Kaderka
+ * @license GNU General Public License, version 2; see LICENSE.txt
+ */
+
+//no direct access
+defined("IN_CMS") or die("Unauthorized access");
+
+/**
+ * Simple paging
+ */
+class Paging
+{
+	/**
+	 * Generate pages navigation
+	 *
+	 * @param int $count number of all items
+	 * @param int $page
+	 * @param int $per_page items per one page
+	 * @param string $url_first first page url
+	 * @param string $url_prep prepared url to insert pagenumber
+	 *
+	 * @return mixed false or array of nav items
+	 */
+	public function genNav($count, $page, $per_page, $url_first, $url_prep) {
+		$nav = false;
+		$pages = ceil($count / $per_page);
+		if ($pages <= 1)
+			return;
+
+		if ($page != 1) {
+			$nav['first'] = $url_first;
+			$nav['prev'] = Url::getFrom($url_prep, $page-1);
+		}
+
+		//calculate minimum page number to show
+		$first = $page - (int) NAV_MAX_COUNT/2;
+		if ($first < 1)
+			$first = 1;
+
+		//if close to the end, show NAV_MAX_COUNT pages
+		$diff = $first + NAV_MAX_COUNT - $pages; //pages over the last page
+		if ($diff > 0)
+			$first = ($first - $diff > 0) ? $first - $diff + 1 : 1;
+
+		for ($i = $first; $i < $first + NAV_MAX_COUNT && $i <= $pages; $i++)
+			$nav['num'][$i] = $i == $page ? false : Url::getFrom($url_prep, $i);
+
+		if ($page < $pages) {
+			$nav['next'] = Url::getFrom($url_prep, $page+1);
+			$nav['last'] = Url::getFrom($url_prep, $pages);
+		}
+		return $nav;
+	}
+
+	/**
+	 * Get first entry to show
+	 *
+	 * @param int $page number of page
+	 * @param int $per_page items per one page
+	 * @param int $count number of all items
+	 *
+	 * @return false or int number of first entry
+	 */
+	public function getFrom($page, $per_page, $count)
+	{
+		$from = ($page - 1)*$per_page;
+		if ($from < 0 || $from > $count - 1) {
+			return false;
+		}
+		return $from;
+	}
+}
